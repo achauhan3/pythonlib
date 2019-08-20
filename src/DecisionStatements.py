ab7accc2e7c1e094066851b24156c42fca782292
ASC <asc@github.com>
2019-08-20 23:05:14 +0530

    updates -
    1. First Module
    2. Decision Statement

---
+++ src/DecisionStatements.py
@@ -0,0 +1,27 @@
+'''
+Created on 20-Aug-2019
+
+@author: Abhishek
+'''
+# if statement implementation
+
+if True:
+    print("True")
+    
+# if-else statement implementation
+
+if True:
+    print("True")
+else:
+    print("False")
+    
+# if-elif-else statement implementation
+a = 5
+if (a == 0):
+    print("0")
+elif (a==1):
+    print("1")
+elif (a==2):
+    print("2")
+else:
+    print("Not Matched - "+str(a))
---
+++ src/FirstModule.py
@@ -0,0 +1,11 @@
+'''
+Created on 11-Aug-2019
+
+@author: Abhishek
+'''
+def add(a,b):
+    print(a+b)
+    return (a+b)
+
+x = add(1,2)
+print(x)
