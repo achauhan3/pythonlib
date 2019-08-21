d3d18570293ae7cb365c8b8557188c1780488fae
ASC <achauhan3@github.com>
2019-08-21 11:16:26 +0530

    ADDED:-
    1. LOOPS
    2. NUMBERS

---
+++ src/Loops.py
@@ -0,0 +1,16 @@
+'''
+Created on 21-Aug-2019
+
+@author: Abhishek
+'''
+
+# while loop
+counter = 10
+while counter >= 1:
+    print(counter)
+    counter -= 1
+    
+# for loop
+print("\n")
+for i in range(10,20,2):
+    print(i)
---
+++ src/Numbers.py
@@ -0,0 +1,9 @@
+'''
+Created on 21-Aug-2019
+
+@author: Abhishek
+'''
+import math
+i = 100
+x = 3
+print(math.exp(x))
