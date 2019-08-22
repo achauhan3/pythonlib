769134f50bbb30563afd180b80e9c23417c2cad8
ASC <achauhan3@github.com>
2019-08-22 11:27:46 +0530

    Strings added

---
+++ src/Strings.py
@@ -0,0 +1,16 @@
+'''
+Created on 21-Aug-2019
+
+@author: Abhishek
+'''
+
+str1 = "I AM THE BEST"
+
+s1 = str1.capitalize()
+print(s1)
+
+s1 = str1.islower()
+print(s1)
+
+s1 = str1.lower()
+print(s1)
